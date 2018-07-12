9c5615e96f2d8a2fab481e6c3317c3049106d0f0
github username <github email address>
2018-07-12 00:07:19 -0400

    setup user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    
+    def index
+        
+    end 
+    
+    def new 
+        
+    end 
+    
+    def create
+    
+    end 
+    
+    def show
+        
+    end 
+    
+    private
+    
+    def user_params
+         params.require(:user).permit(:name, :age, :email, :admin, :password)
+    end
+    
+    def logged_in
+        
+    end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :workspaces, through: :bookings
     
     has_secure_password
+    
 end
