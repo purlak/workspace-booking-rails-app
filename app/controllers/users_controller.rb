ebcb43a30f88230cc06f06f4111981dd88b82c4a
github username <github email address>
2018-07-12 00:31:58 -0400

    updated create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,13 @@ class UsersController < ApplicationController
     end 
     
     def create
-    
+        @user = User.new(user_params)
+        if user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            redirect_to '/signup'
+        end 
     end 
     
     def show
