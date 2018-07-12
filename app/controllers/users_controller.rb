56973d6b5c186225d1c35e00a3a0aea4a15acb33
github username <github email address>
2018-07-12 00:08:31 -0400

    setup before action and skip before controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :logged_in
+    skip_before_action :logged_in, only: [:new, :create]
     
     def index
         
@@ -23,6 +25,9 @@ class UsersController < ApplicationController
     end
     
     def logged_in
-        
+        if !session[:user_id]
+            redirect_to '/'
+        end     
     end 
+    
 end
