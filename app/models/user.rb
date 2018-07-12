22a254b252ed9e33a8cb0baa296bebf13b27b45b
github username <github email address>
2018-07-12 10:32:24 -0400

    setup login routes in sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
     
     has_secure_password
     
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { in: 6..20 }
+    
 end
