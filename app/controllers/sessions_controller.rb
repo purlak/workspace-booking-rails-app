f61ca01e0bd3236a3c2e8b177ae1213ce7b0f0fb
github username <github email address>
2018-07-12 00:53:50 -0400

    setup sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    
+    def new
+        
+    end 
+    
+    def create
+    
+    end
+    
+    def show
+        
+    end 
+    
+    private
+    
+    def user_params
+         params.require(:user).permit(:name, :age, :email, :admin, :password)
+    end
 end
