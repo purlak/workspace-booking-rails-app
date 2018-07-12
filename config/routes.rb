93933c2ae557f7446512b047e596912942f69d88
github username <github email address>
2018-07-11 23:00:22 -0400

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users
+    resources :workspaces
+    resources :buildings
+    resources :bookings, only: [:create]
+    
+    root "application#home"
+
 end
