1e832042097962c6591c335dad89870d2030271e
github username <github email address>
2018-07-12 10:58:00 -0400

    setup root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    root to: "application#index"
+    
     resources :users
     resources :workspaces
     resources :buildings
     resources :bookings, only: [:create]
-    
-    root "application#home"
     
     #Signup routes
     get '/signup', to: 'users#new'
