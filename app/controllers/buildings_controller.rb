4de79d7f429680dbad8ace4efd2a26f74d9c5f95
github username <github email address>
2018-07-12 22:49:49 -0400

    added new and create actions and strong params

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,27 @@
 class BuildingsController < ApplicationController
+    
+    def index 
+        @buildings = Building.all
+    end 
+    
+    def new
+        @building = Building.new
+    end 
+    
+    def create
+        @building =  Building.find_by(params[:id])
+        if @building.save
+            redirect_to @building
+            
+        else 
+            render :new 
+        end 
+    end 
+    
+    private
+    
+    def building_params
+        params.require(:building).permit(:location_name, :address)
+    end 
+    
 end
