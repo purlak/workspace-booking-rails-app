adddf6df6f9465124567441819290ecc0a159280
github username <github email address>
2018-07-13 11:14:53 -0400

    added current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
     def index 
         
     end 
+    
+    def current_user
+        current_user = User.find_by(id: session[:user_id])
+    end 
 end
