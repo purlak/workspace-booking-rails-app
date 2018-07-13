434b116163969d1dcfcf71fbb0a5bd8e6eb1824b
github username <github email address>
2018-07-12 22:52:54 -0400

    added new and create actions and strong params

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,2 +1,25 @@
 class WorkspacesController < ApplicationController
+    def index 
+        @workspaces = Workspace.all
+    end 
+    
+    def new
+        @workspace = Workspace.new
+    end 
+    
+    def create
+        @workspace =  Workspace.find_by(params[:id])
+        if @workspace.save
+            redirect_to @workspace
+            
+        else 
+            render :new 
+        end 
+    end 
+    
+    private
+    
+    def workspace_params
+        params.require(:building).permit(:workspace_type, :availability, :building_id)
+    end 
 end
