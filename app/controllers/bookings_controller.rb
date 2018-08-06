5945e72bc8939339ced34b3eadfe5e0d23a15c04
github username <purnima.nyc@gmail.com>
2018-08-06 15:03:18 -0400

    update bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,34 @@
 class BookingsController < ApplicationController
+    
+    def index 
+        @bookings = Booking.all
+    end 
+    
+    def new
+        @workspace = Workspace.find_by(id: params[:workspace_id])
+        @booking = Booking.new
+        #binding.pry
+    end 
+    
+    def create
+        #@building = Building.find_by(id: params[:workspace][:building_id])
+        #binding.pry
+        @workspace = Workspace.find_by(id: params[:workspace_id])
+        @booking = Booking.create(id: params[:id])
+        
+        binding.pry
+         
+        if @booking.save
+               redirect_to @booking     
+        else 
+            render :new 
+        end 
+    end 
+    
+    def show
+        
+    end 
+    
+    private
+    
 end
